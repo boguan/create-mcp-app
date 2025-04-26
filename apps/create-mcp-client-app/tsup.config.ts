c62accaa597f42ced6f95a8354c12a9f93e36db4
BoGuan <786506@gmail.com>
2025-04-26 17:19:46 +0800

    refactor: simplify package and build configurations

--- apps/create-mcp-client-app/tsup.config.ts
+++ apps/create-mcp-client-app/tsup.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from "tsup";
-import { resolve } from "path";
 
 const isDev = process.env.npm_lifecycle_event === "dev";
 
 export default defineConfig({
   clean: true,
   entry: ["src/index.ts"],
+  noExternal: ["@mcp/cli"],
   format: ["esm"],
   minify: false, // !isDev
   platform: 'node',
   target: "esnext",
   outDir: "dist",
-  onSuccess: isDev ? "node dist/index.js" : undefined,
-  esbuildOptions(options) {
-    options.alias = {
-      "@mcp/cli": resolve(__dirname, "../../packages/cli/dist/index.js")
-    }
-  }
+  onSuccess: isDev ? "node dist/index.js" : undefined
 });
